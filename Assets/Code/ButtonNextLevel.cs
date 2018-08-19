caa42abb77059a3544460e974a2b4a24058730aa
klamomd <35202056+klamomd@users.noreply.github.com>
2018-08-19 13:04:10 -0600

    Ads are implemented!
    TODO:
    - Android back button
    - Multiple scenes
    - Change organizationId?
    - Customize ad settings
    - Android store icons/assets
    - Improve current icon
    - Uhhhhhh

--- Assets/Code/ButtonNextLevel.cs
+++ Assets/Code/ButtonNextLevel.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
 
 public class ButtonNextLevel : MonoBehaviour
 {
+    private int nextScene = 0;
+
     public void NextLevelButton(int index)
     {
-        SceneManager.LoadScene(index);
+        //SceneManager.LoadScene(index);
+        if (index % 3 == 0)
+        {
+            var options = new ShowOptions { resultCallback = AfterAdLoadScene };
+            nextScene = index;
+            if (Advertisement.IsReady()) Advertisement.Show(options);
+        }
+        else SceneManager.LoadScene(index);
+
     }
 
     public void NextLevelButton(string levelName)
@@ -15,6 +26,20 @@ public class ButtonNextLevel : MonoBehaviour
     }
 
     public void MainMenuButton()
+    {
+        var options = new ShowOptions { resultCallback = AfterAdLoadMainMenu };
+        if (Advertisement.IsReady()) Advertisement.Show(options);
+        //SceneManager.LoadScene(0);
+
+    }
+
+    private void AfterAdLoadScene(ShowResult result)
+    {
+        SceneManager.LoadScene(nextScene);
+        nextScene = 0;
+    }
+
+    private void AfterAdLoadMainMenu(ShowResult result)
     {
         SceneManager.LoadScene(0);
     }
